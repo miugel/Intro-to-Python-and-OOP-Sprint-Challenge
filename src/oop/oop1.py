87aaf19bf8d6432b1c4d3a2ddcc22ddecf1be83d
Miguel <miguelqnicolas@gmail.com>
2020-04-10 11:23:38 -0400

    completed oop exercises

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -1,5 +1,5 @@
-# Write classes for the following class hierarchy:
-#
+# write classes for the following class hierarchy:
+
 #  [Vehicle]->[FlightVehicle]->[Starship]
 #      |                |
 #      v                v
@@ -7,13 +7,29 @@
 #   |       |
 #   v       v
 # [Car]  [Motorcycle]
-#
-# Each class can simply "pass" for its body. The exercise is about setting up
-# the hierarchy.
-#
-# e.g.
-#
-# class Whatever:
-#     pass
-#
-# Put a comment noting which class is the base class
+
+# each class can simply 'pass' for its body 
+# the exercise is about setting up the hierarchy
+# put a comment noting which class is the base class
+
+# base class
+class Vehicle:
+    pass
+
+class FlightVehicle(Vehicle):
+    pass
+
+class Starship(FlightVehicle):
+    pass
+
+class GroundVehicle(Vehicle):
+    pass
+
+class Airplane(FlightVehicle):
+    pass
+
+class Car(GroundVehicle):
+    pass
+
+class Motorcycle(GroundVehicle):
+    pass
--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -1,23 +1,23 @@
-# To the GroundVehicle class, add method drive() that returns "vroooom".
-#
-# Also change it so the num_wheels defaults to 4 if not specified when the
-# object is constructed.
+# to the GroundVehicle class, add method drive() that returns 'vroooom'
+# also change it so the num_wheels defaults to 4 if not specified when the object is constructed
 
 class GroundVehicle():
-    def __init__(self, num_wheels):
+    def __init__(self, num_wheels=4):
         self.num_wheels = num_wheels
 
-    # TODO
+    def drive(self):
+        return 'vroooom'
 
+# subclass Motorcycle from GroundVehicle
+# make it so when you instantiate a Motorcycle, it automatically sets the number of wheels to 2 by passing that to the constructor of its superclass
+# override the drive() method in Motorcycle so that it returns 'BRAAAP!!'
 
-# Subclass Motorcycle from GroundVehicle.
-#
-# Make it so when you instantiate a Motorcycle, it automatically sets the number
-# of wheels to 2 by passing that to the constructor of its superclass.
-#
-# Override the drive() method in Motorcycle so that it returns "BRAAAP!!"
+class Motorcycle(GroundVehicle):
+    def __init__(self):
+        super().__init__(2)
 
-# TODO
+    def drive(self):
+        return 'BRAAAP!!'
 
 vehicles = [
     GroundVehicle(),
@@ -27,6 +27,7 @@ vehicles = [
     Motorcycle(),
 ]
 
-# Go through the vehicles list and print the result of calling drive() on each.
+# go through the vehicles list and print the result of calling drive() on each
 
-# TODO
+for item in vehicles:
+    print(item.drive())
