73128541ed321d5eadd55a07cc452a4d5246108f
Miguel <miguelqnicolas@gmail.com>
2020-04-10 11:41:02 -0400

    completed comp exercises

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -1,71 +1,66 @@
-# The following list comprehension exercises will make use of the 
-# defined Human class. 
+# the following list comprehension exercises will make use of the defined Human class
 class Human:
     def __init__(self, name, age):
         self.name = name
         self.age = age
 
     def __repr__(self):
-        return f"<Human: {self.name}, {self.age}>"
+        return f'<Human: {self.name}, {self.age}>'
 
 humans = [
-    Human("Alice", 29),
-    Human("Bob", 32),
-    Human("Charlie", 37),
-    Human("Daphne", 30),
-    Human("Eve", 26),
-    Human("Frank", 18),
-    Human("Glenn", 42),
-    Human("Harrison", 12),
-    Human("Igon", 41),
-    Human("David", 31),
+    Human('Alice', 29),
+    Human('Bob', 32),
+    Human('Charlie', 37),
+    Human('Daphne', 30),
+    Human('Eve', 26),
+    Human('Frank', 18),
+    Human('Glenn', 42),
+    Human('Harrison', 12),
+    Human('Igon', 41),
+    Human('David', 31),
 ]
 
-# Write a list comprehension that creates a list of names of everyone
-# whose name starts with 'D':
-print("Starts with D:")
-a = []
+# write a list comprehension that creates a list of names of everyone whose name starts with 'D'
+print('Starts with D:')
+a = [item.name for item in humans if item.name[0] == 'D']
 print(a)
 
-# Write a list comprehension that creates a list of names of everyone
-# whose name ends in "e".
-print("Ends with e:")
-b = []
+# write a list comprehension that creates a list of names of everyone whose name ends in 'e'
+print('Ends with e:')
+b = [item.name for item in humans if item.name[-1] == 'e']
 print(b)
 
-# Write a list comprehension that creates a list of names of everyone
-# whose name starts with any letter between 'C' and 'G' inclusive.
-print("Starts between C and G, inclusive:")
-c = []
+# write a list comprehension that creates a list of names of everyone whose name starts with any letter between 'C' and 'G' inclusive
+print('Starts between C and G, inclusive:')
+c = [item.name for item in humans if item.name[0] in ['C', 'D', 'E', 'F', 'G']]
 print(c)
+# most efficient?
 
-# Write a list comprehension that creates a list of all the ages plus 10.
-print("Ages plus 10:")
-d = []
+# write a list comprehension that creates a list of all the ages plus 10
+print('Ages plus 10:')
+# d = [item.age for item in humans if item.age > 10]
+d = [item.age + 10 for item in humans]
 print(d)
 
-# Write a list comprehension that creates a list of strings which are the name
-# joined to the age with a hyphen, for example "David-31", for all humans.
-print("Name hyphen age:")
-e = []
+# write a list comprehension that creates a list of strings which are the name joined to the age with a hyphen for all humans
+print('Name hyphen age:')
+e = [f'{item.name}-{item.age}' for item in humans]
 print(e)
 
-# Write a list comprehension that creates a list of tuples containing name and
-# age, for example ("David", 31), for everyone between the ages of 27 and 32,
-# inclusive.
-print("Names and ages between 27 and 32:")
-f = []
+# write a list comprehension that creates a list of tuples containing name and age for everyone between the ages of 27 and 32 inclusive
+print('Names and ages between 27 and 32:')
+f = [(item.name, item.age) for item in humans if item.age >= 27 and item.age <= 32]
 print(f)
+# correct?
 
-# Write a list comprehension that creates a list of new Humans like the old
-# list, except with all the names uppercase and the ages with 5 added to them.
-# The "humans" list should be unmodified.
-print("All names uppercase:")
-g = []
+# write a list comprehension that creates a list of new Humans like the old list, except with all the names uppercase and the ages with 5 added to them
+# the 'humans' list should be unmodified
+print('All names uppercase:')
+g = [Human(item.name.upper(), item.age + 5) for item in humans]
 print(g)
 
-# Write a list comprehension that contains the square root of all the ages.
-print("Square root of ages:")
+# write a list comprehension that contains the square root of all the ages
+print('Square root of ages:')
 import math
-h = []
+h = [math.sqrt(item.age) for item in humans]
 print(h)
